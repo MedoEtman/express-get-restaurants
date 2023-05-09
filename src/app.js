e673a59e40c758305083d0cea5c41d009fdfdf6a
MedoEtman <medo-ammar@hotmail.co.uk>
2023-05-09 15:05:20 +0100

    WIP

--- src/app.js
+++ src/app.js
@@ -3,9 +3,17 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
+app.set("json spaces", 2);
+
 //TODO: Create your GET Request Route Below: 
 
-
-
+app.get("/restaurants", async (req, res, next) => {
+    try {
+        const restaurants = await Restaurant.findAll();
+        res.json(restaurants);
+    } catch (error) {
+        next(error);
+    }
+});
 
 module.exports = app;
