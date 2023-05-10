5452adfc90a9d10a103ebe63e3c8f1fb7e616ea0
MedoEtman <medo-ammar@hotmail.co.uk>
2023-05-10 21:12:33 +0100

    WIP

--- src/app.js
+++ src/app.js
@@ -4,31 +4,48 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 app.set("json spaces", 2);
+app.use(express.json());
 
 //TODO: Create your GET Request Route Below: 
 
-app.post("/restaurants", async (req, res, next) => {
-    try {
-        const restaurants = await Restaurant.findAll();
-        res.json(restaurants);
-    } catch (error) {
-        next(error);
-    }
-})
+a//app.use(express.urlencoded()); // parse the request body with urlencoded values
 
-app.get("/restaurants", async (req, res, next) => {
-    try {
-        const restaurants = await Restaurant.findAll();
-        res.json(restaurants);
-    } catch (error) {
-        next(error);
-    }
+app.get("/restaurants",  async (request, response, next)=>{
+    const restaurants = await Restaurant.findAll();
+    response.json(restaurants);
 });
 
+app.get("/restaurants/:id", async (request, response, next) => {
+    const index = request.params.id 
+    const restaurantAtIndex = await Restaurant.findByPk(index);
+    response.json(restaurantAtIndex);
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send("something broke!");
+});
+// express route to add a new restaurant to database
+
+app.post("/restaurants", async (request, response, next) => {
+    const restaurant = request.body;
+    await Restaurant.create(restaurant);
+    const restaurants = await Restaurant.findAll();
+    response.json(restaurants);
+})
+// express route to update(replace) a restaurant in database
+app.put("/restaurants/:id", async (request, response, next) => {
+    const index = request.params.id 
+    const restaurantAtIndex = await Restaurant.findByPk(index);
+    const newRestaurant = request.body;
+    await restaurantAtIndex.update(newRestaurant);
+    const restaurants = await Restaurant.findAll();
+    response.json(restaurants);
+})
+
+// express route to delete a restaurant in given index in database
+app.delete("/restaurants/:id", async (request, response, next) => {
+    const index = request.params.id 
+    const restaurantAtIndex = await Restaurant.findByPk(index);
+    await restaurantAtIndex.destroy();
+    const restaurants = await Restaurant.findAll();
+    response.json(restaurants);
 });
 
 module.exports = app;
--- src/app.spec.js
+++ src/app.spec.js
@@ -11,4 +11,13 @@ it("returns an array", async () => {
     expect(response.body[0]).toHaveProperty("location");
     expect(response.body[0]).toHaveProperty("cuisine");
   });
+
+  it('successfully adds a restaurant to the database', async () => {
+    const payload = {
+        name: "Nandos",
+        location: "Everywhere",
+        cuisine: "Fastfood"
+    };
+    const response = (await request(app).post("/restaurants")).setEncoding(payload)
+  })
 });
